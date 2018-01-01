36f83c6d37998f69b5f42cbb710a0a10cf32fe2b
Mahesh Kumar B S <maheshk2@cisco.com>
2018-01-01 15:20:02 +0530

    Version 1 : Few changes to grid
    1. Added few filters
    2. Converted Recommended Date from String to Date. (Not able to display
    Time in dd—MMM-YYYY) formate, maybe the right way to display is using
    Pipes…
    3. Added Days (duration of recommendation), is the right design to use
    Pipe’s here? Even the Data type is number, the sorting is not working
    4. Updated Product backlog with few new ideas.

--- wealth-management-client/src/app/modal/stock.ts
+++ wealth-management-client/src/app/modal/stock.ts
@@ -1,25 +1,35 @@
 export class Stock {
   script: string;
   name: string;
-  buyPrice: number;
+  investPrice: number;
   targetPrice: number;
   rcomPrice: number;
   rcomDate: Date;
   rcomTimeScale: string;
   rcomBy: string;
+  rtargetPercentage: number;
+  days: number;
 
   constructor() {
 
   }
 
-  setData(data: any) : void {
+  setData(data: any): void {
+    var firstDate = new Date(data.rcomDate);
+    var secondDate = new Date();
     this.script = data.script;
     this.name = data.name;
-    this.buyPrice = data.buyPrice;
-    this.targetPrice = data.targetPrice;
+    this.investPrice = data.buyPrice;
+
     this.rcomPrice = data.rcomPrice;
-    this.rcomDate = data.rcomDate;
+    this.targetPrice = data.targetPrice;
+    this.rtargetPercentage = (((data.targetPrice - data.rcomPrice) / data.rcomPrice) * 100).toFixed(2);
+
+    this.rcomDate = new Date(data.rcomDate);
+    this.days = Math.abs((firstDate.valueOf() - secondDate.valueOf()) / (24 * 60 * 60 * 1000)).toFixed(0);
+
     this.rcomTimeScale = data.rcomTimeScale;
     this.rcomBy = data.rcomBy;
+
   }
 }
