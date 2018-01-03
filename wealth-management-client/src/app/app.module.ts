45fb79be5e8d769a2c681c5516df30a815682017
Narasimhan Gopinath <nagopina@cisco.com>
2018-01-03 22:39:40 +0530

    Recommendations - Grid representation implemented

--- wealth-management-client/src/app/app.module.ts
+++ wealth-management-client/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReportsModule } from './reports/reports.module';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppSideMenuBarComponent } from './app-sidemenubar/app-sidemenubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppSideMenuBarComponent
   ],
   imports: [
     BrowserModule,
-    ReportsModule
+    ReportsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
