58e4b2cb13b1acc6329b3ce47aebe70f0b2682cc
Mahesh Kumar B S <maheshk2@cisco.com>
2018-03-10 17:30:31 +0530

    Splitting Recommendations and Investment into 2 different files.

--- start-application.sh
+++ start-application.sh
@@ -1,6 +1,21 @@
-#start the data server
+echo "Starting Wealth Builder Data Server"
+echo "Prerequisites:"
+echo "node and npm installations should be available"
+echo "=========== Creating Data for the Server =========== "
+if [ ! -e data-server/datasource.json ]
+then
+    echo "{">> data-server/datasource.json
+    echo "\"recommendations\" : " >> data-server/datasource.json
+    cat data-server/recommendations.json >> data-server/datasource.json
+    echo ",\"investments\" : " >> data-server/datasource.json
+    cat data-server/investments.json >> data-server/datasource.json
+    echo "}">> data-server/datasource.json
+fi
+echo "=========== Data creation complete =========== "
 nohup ./data-server/bin/index.js --watch data-server/datasource.json --port 3030 &
+echo "*********** Wealth Builder Data Server Started *********** "
 
-#start the application
+echo "*********** Starting Wealth Builder Application *********** "
 cd open-wealth-manager
 npm start  
+echo "*********** Wealth Builder Application Started *********** "
